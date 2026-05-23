212b57e26de5001e947f465087cc9bdec12bc420
Rama U <ramarangeswarareddy123@gmail.com>
2026-05-23 22:12:51 +0530

    database connecting and vault reading properties fixed

---
+++ src/main/java/com/kgpalle/moneymind/controller/UserDetails.java
@@ -0,0 +1,25 @@
+package com.kgpalle.moneymind.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserDetails {
+
+    @Value("${username}")
+    private String username;
+
+    @Value("${password}")
+    private String password;
+
+    @GetMapping("/user-details")
+    public String getUserDetails() {
+        // Placeholder for actual user details retrieval logic
+        return "User details will be displayed here." + "\nUsername: " + username + "\nPassword: " + password;
+    }
+    
+}
---
+++ src/main/java/com/kgpalle/moneymind/entity/Student.java
@@ -0,0 +1,5 @@
+package com.kgpalle.moneymind.entity;
+
+//@Entity
+public class Student {
+}
